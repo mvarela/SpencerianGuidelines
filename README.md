187c6f467b04c317ee418ae4c23f649481c41b0e
Martin Varela <mvr.rennes@gmail.com>
2014-03-03 14:23:22 +0200

    Cleaned up a bit, integrated Coppeplate guidelines generation

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 SpencerianGuidelines
 ====================
 
-Simple Prawn-powered ruby script for generating guidelines for Spencerian penmanship
+Simple Prawn-powered ruby script for generating guidelines for Spencerian and Coppeplate penmanship
 
 I'm putting this out AS IS, and while it could use several improvements, I don't have much time for it at the moment.
 
@@ -9,3 +9,14 @@ In order to use it you need a working ruby installation, as well as the Prawn ge
 
 The examples folder contains a few pre-generated sheets, in case you don't have a ruby installation handy.
 
+Call as:
+    ./spencerian_guidelines {s|c} x_height slant
+
+ For example:
+    ./spencerian_guidelines s 4 52
+
+ will produce Spencerian guidelines with a 4mm x-height and 52 degree slant.
+
+    ./spencerian_guidelines c 6 55
+
+will produce Copperplate-style guidelines with 6mm x-height and 55 degree slant.
--- spencerian_guidelines.rb
+++ spencerian_guidelines.rb
@@ -2,22 +2,29 @@
 require 'prawn'
 require 'prawn/measurement_extensions'
 
-def stave (x_height, page_width, cursor, main_slant)
+class Prawn::Document  
+MAIN_LINE_COLOR  = "000000"
+LIGHT_LINE_COLOR = "aaaaaa"
+SLANT_LINE_COLOR = "aaaaff"
+JOIN_LINE_COLOR  = "ffaaaa"
+
+
+def spencerian_stave (x_height, page_width, cursor, main_slant)
 	separation = 15
 	slant = (main_slant/180.0)*Math::PI
 	join_slant = ((main_slant-22)/180.0)*Math::PI
-	stroke_color "000000" 
+	stroke_color MAIN_LINE_COLOR
 	stroke do
 		horizontal_line 0, page_width, :at => cursor
 		horizontal_line 0, page_width, :at => (cursor + x_height)
 	end
-	stroke_color "aaaaaa" 
+	stroke_color LIGHT_LINE_COLOR
 	stroke do
 		horizontal_line 0, page_width, :at => (cursor + 3*x_height)
 		horizontal_line 0, page_width, :at => (cursor - 2*x_height)
 	end
 	stroke do
-		stroke_color "aaaaff" 
+		stroke_color SLANT_LINE_COLOR
 		horizontal_line 0, page_width, :at => (cursor + 2*x_height)
 		horizontal_line 0, page_width, :at => (cursor - x_height)
 	end
@@ -29,38 +36,95 @@ def stave (x_height, page_width, cursor, main_slant)
 		x2 = x1 + (5*x_height / Math::tan(slant))
 		stroke do
 			line [x1,y1], [x2,y2]
-		end	
+		end
 	end
-	stroke_color "ffaaaa" 
+	stroke_color JOIN_LINE_COLOR
 	join_slant_lines = (page_width / separation.mm).to_i
 	y2 = cursor + 3*x_height
 	join_slant_lines.times do |i|
 		x1 = (separation*i).mm + (2*x_height / Math::tan(slant))
-		y1 = cursor 
+		y1 = cursor
 		x2 = x1 + (3*x_height / Math::tan(join_slant))
 		stroke do
 			line [x1,y1], [x2,y2]
-		end	
+		end
 	end
 end
 
-def do_page(page_height, page_width, x_height, slant)
-	font_size(9)
-	text "Spencerian #{x_height/1.mm}mm, #{slant} degrees"
-	self.line_width = 0.1.mm
-	move_down 2.cm
-	stave_height = 5.5 * x_height
-	total_staves = (page_height / stave_height).to_i - 1
-	total_staves.times do |i|
-		stave(x_height, page_width, i*stave_height, slant)
-		move_down stave_height
+
+def copperplate_stave (x_height, page_width, cursor, main_slant)
+	slant = (main_slant/180.0)*Math::PI
+	separation = x_height / (2 * Math::tan(slant))
+	stroke_color MAIN_LINE_COLOR
+	stroke do
+		horizontal_line 0, page_width, :at => cursor
+		horizontal_line 0, page_width, :at => (cursor + x_height)
 	end
+	stroke_color LIGHT_LINE_COLOR
+	stroke do
+		horizontal_line 0, page_width, :at => (cursor + 2.5*x_height)
+		horizontal_line 0, page_width, :at => (cursor - 1.5*x_height)
+	end
+	stroke do
+		stroke_color SLANT_LINE_COLOR
+		horizontal_line 0, page_width, :at => (cursor + 1.75*x_height)
+		horizontal_line 0, page_width, :at => (cursor - 0.75*x_height)
+	end
+	slant_lines = (page_width / separation.mm).to_i - 1
+	y2 = cursor + 2.5*x_height
+	slant_lines.times do |i|
+		x1 = (separation*i).mm
+		y1 = cursor - 1.5*x_height
+		x2 = x1 + (4*x_height / Math::tan(slant))
+		stroke do
+			line [x1,y1], [x2,y2]
+		end
+	end
+end
 
 end
 
+def do_page(page_height, page_width, x_height, slant, style)
+  page_height = page_height.mm
+  page_width = page_width.mm
+  x_height = x_height.mm
+  case style
+  when :spencerian
+    title = "Spencerian"
+    multiplier = 5.5
+  when :copperplate
+    title = "Copperplate"
+    multiplier = 4.5
+  else
+    exit(-1)
+  end
+  Prawn::Document.generate("#{title}_#{x_height/1.mm}mm_#{slant} degrees.pdf",{:page_size=>"A4"}) do
+    font_size(9)
+    text "#{title} #{x_height/1.mm}mm, #{slant} degrees"
+    self.line_width = 0.1.mm
+    move_down 2.cm
+    stave_height = multiplier * x_height
+    total_staves = ((page_height - 2.cm) / stave_height).to_i - 1
+    total_staves.times do |i|
+      case style
+      when :spencerian then spencerian_stave(x_height, page_width, i*stave_height, slant)
+      when :copperplate then copperplate_stave(x_height, page_width, i*stave_height, slant)
+      end
+      move_down stave_height
+    end
+  end
+end
 
-x_height = ARGV[0]
-slant = ARGV[1]
-Prawn::Document.generate("Spencerian_#{x_height}mm_#{slant} degrees.pdf", {:page_size=>"A4"} ) do
-	do_page	290.mm, 185.mm, x_height.to_f.mm, slant.to_f
-end	
+argstyle = ARGV[0]
+x_height = ARGV[1]
+slant    = ARGV[2]
+
+style = case argstyle
+      when "c" then :copperplate
+      when "s" then :spencerian
+      else nil
+      end
+exit unless style
+puts x_height.to_f
+do_page 290, 185, x_height.to_f, slant.to_f, style
+
